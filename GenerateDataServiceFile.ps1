824c4d6579de2126672781a2ff8a4da25c4620f4
Rob Reynolds <ferventcoder@gmail.com>
2016-01-14 08:06:05 -0600

    (maint) add local web to GenerateDataServiceFile

--- GenerateDataServiceFile.ps1
+++ GenerateDataServiceFile.ps1
@@ -4,3 +4,4 @@ $dataSvcUtil = 'C:\Program Files (x86)\Microsoft WCF Data Services\5.6\bin\tools
 $serviceFile = "$thisDir\src\chocolatey.package.cleanup\infrastructure.app\webservices\ChocolateySubmittedFeedService.cs"
 
 . $dataSvcUtil /out:"$serviceFile" /version:2.0 /language:csharp /nologo /uri:https://chocolatey.org/api/v2/submitted/
+#. $dataSvcUtil /out:"$serviceFile" /version:2.0 /language:csharp /nologo /uri:http://localhost:55881/api/v2/submitted/
